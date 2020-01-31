aecebe57d381a8531a237baf797ab6199121487e
Daniel Niko <daniel.gultom@gmail.com>
2020-01-31 16:03:05 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  baseUrl: string = 'https://crudbudget.herokuapp.com/api/';
+  categories: any;
+
+  constructor(private http: HTTP, private httpClient: HttpClient) {}
+
+  ionViewWillEnter() {
+
+    this.http.setServerTrustMode('pinned').then(() => {
+    }, function(err) {
+
+    });
+
+
+
+    let requestUrl = this.baseUrl + "categories";
+    let requestData = {};
+    let headers = {}
+    if (window.cordova) {
+      // if run in device use advanced-http
+      this.http.get(requestUrl, requestData, headers).then(
+        response => {
+          console.log(response.data);
+        },
+        error => {
+        }
+      )
+    } else {
+      // if run in browser use normal http
+      this.httpClient.get(requestUrl, headers).subscribe(
+        (data: any) => {
+        if (data) {
+          this.categories = data._embedded.categories;
+          console.log(this.categories);
+        }
+      })
+    }
+  }
+
+  doInquiry() {
+    
+  }
 
 }
